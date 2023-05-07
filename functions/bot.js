f3651509ce5a28f3bf4a2955ec5bb8726195302f
Habtamu <habtamuadugna.d@gmail.com>
2023-05-07 13:35:53 +0300

    bot.webhookCallback()

--- functions/bot.js
+++ functions/bot.js
@@ -1,27 +1,28 @@
-const { Telegraf } = require('telegraf');
 require('dotenv').config();
-
+const { Telegraf } = require('telegraf');
 const serverless = require('serverless-http');
+
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 const web_link = "https://asbeza-order-webapp-telegram.netlify.app/";
 
 const bot = new Telegraf(BOT_TOKEN);
+
+
 bot.start((ctx) => ctx.reply('Welcome',{
     reply_markup:{inline_keyboard:[[{text: "Click Here", web_app: {url: web_link}}]]},
-}));
+})); 
 
-bot.on('message', (ctx) => {
-    const message = ctx.message;
-    const data = JSON.parse(message.web_app_data.data);
-    ctx.reply('Thank You. Your Order: '+ data.text);
-});
+bot.on('message', async (ctx) => {
+    try {
+        const message = ctx.message;
+        const data = JSON.parse(message.web_app_data.data);
+        await ctx.reply('Thank you. Your Order: ' + data.text);
+    } catch (err) {
+        console.error(err);
+        await ctx.reply('Oops, something went wrong!');
+    }
+}); 
 
-//bot.launch();  
-
-module.exports.handler = async (event, context) => {
-    console.log('function triggered');
-    return serverless(bot)(event, context);
-  };
-
+module.exports.handler = serverless(bot.webhookCallback());
